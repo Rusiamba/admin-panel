1cb1969d107006e9c3c127d4751f8042e3d82de8
Ruslan <rus.orudjev@mail.ru>
2021-06-26 00:06:45 +0300

    Создание store-файлов

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="header">
-    <div class="header__menu" v-on:click="clickMenu">
-      <img src="https://img.icons8.com/fluent/48/000000/menu--v2.png"/>
-    </div>
     <h1 style="color: white">Личный Кабинет</h1>
   </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    store() {
-      return this.$store.state.header.open.openRoll;
-    },
-  },
-  methods: {
-    clickMenu(store) {
-      this.$store.commit('diff');
-    }
-  }
-}
+export default {}
 </script>
 
 <style scoped>
